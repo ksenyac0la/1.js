ee25565defc71fbf02205e71079e10b539c0c96a
ksenyac0la <99069503+ksenyac0la@users.noreply.github.com>
2022-04-26 14:40:24 +0300

    removed

--- 5(2).js
+++ 5(2).js
@@ -1,10 +1,8 @@
 function reverseString (str) {
     let newStr = "";
-   
   for(let i = str.length - 1; i >= 0; i--){
     newStr += str[i]
   }
-      return newStr;
-  
+     return newStr;
   }
   reverseString("Я ни хера не понимаю циклы");
--- 5.js
+++ 5.js
@@ -1,7 +1,6 @@
 function reverseString(str) {
-
     return str.split("").reverse();
-  
   }
   reverseString("Я ни хера не понимаю циклы");
+  
   
